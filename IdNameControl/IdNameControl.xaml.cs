69d33f5706e8ced88448d887ffe187a1cb836cec
Julian <moshammer.ju@gmail.com>
2019-11-02 17:50:45 +0100

    initial commit

---
+++ IdNameControl/IdNameControl.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IdNameControl
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class IdNameControl : UserControl
+    {
+        private string id;
+
+        public string Id
+        {
+            get { return id; }
+            set 
+            { 
+                id = value;
+                lblId.Content = id; 
+            }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set 
+            {
+                name = value;
+                lblId.Content = name; 
+            }
+        }
+
+
+
+        public IdNameControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
